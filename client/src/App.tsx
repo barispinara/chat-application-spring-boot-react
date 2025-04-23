8bba003136f2d9982a36b3f4a3d50fb59a99ea05
barispinara <barispinara@gmail.com>
2025-04-23 16:47:52 +0300

    #25 Global styles is added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,24 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./redux/store";
 import { AppRoutes } from "./router/AppRoutes";
+import { GlobalStyles } from "@mui/material";
 
 function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <AppRoutes/>
-      </BrowserRouter>
-    </Provider>
-    
+    <>
+      <GlobalStyles
+        styles={{
+          html: { height: "100%" },
+          body: { height: "100%", margin: 0 },
+          "#root": { height: "100%" },
+        }}
+      />
+      <Provider store={store}>
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      </Provider>
+    </>
   );
 }
 
